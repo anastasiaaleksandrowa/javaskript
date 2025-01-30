ab0f092514b0cb5cd82a259a87c9e040ad79c109
Kira <kirnan_t@outlok.com>
2025-01-30 21:00:03 +0700

    task3

---
+++ task1.js
@@ -0,0 +1,17 @@
+
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+};
+
+// Проходим по всем значениям объекта
+for (let key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
